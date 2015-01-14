c02cd42017704810e8b61fb01be7aa9bc4f676ca
igoryen <igor.yentaltsev@gmail.com>
2015-01-14 16:10:14 -0500

    pushed initial docs

---
+++ gryphtech_a/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gryphtech_a.MvcApplication" Language="C#" %>
---
+++ gryphtech_a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gryphtech_a.Startup))]
+namespace gryphtech_a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
